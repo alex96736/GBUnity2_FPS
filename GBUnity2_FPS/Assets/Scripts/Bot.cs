6c4df205789821f321909489ba06d7e1282e00f1
ALeX96736 <alex96736@yandex.ru>
2019-08-30 12:34:18 +0300

    AR

---
+++ GBUnity2_FPS/Assets/Scripts/Bot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : Unit
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        Health = 100;
+    }
+
+    void Update()
+    {
+        
+    }
+}
