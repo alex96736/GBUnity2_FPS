c5c51ace5a5d9071a32df3dc07e6e4abbbe4442f
ALeX96736 <alex96736@yandex.ru>
2019-09-06 09:07:42 +0300

    AR

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : BaseObject, ISetDamage
+public class Unit : BaseObject, ISetDamage, IGetHealth
 {
+    // текущее здоровье персонажа
     [SerializeField] private int _health;
+    // базовое здоровье персонажа
+    private int _baseHealth;
+
 
     public int Health { get => _health; set => _health = value; }
+    public int BaseHealth { get => _baseHealth; set => _baseHealth = value; }
+
 
     public void SetDamage(int damage)
     {
-        if (_health > 0)
+        if (_health > damage)
         {
             _health -= damage;
             Debug.Log($"SetDamage: {damage}");
@@ -21,5 +27,14 @@ public class Unit : BaseObject, ISetDamage
             Destroy(_GameObject);
         }
         Debug.Log($"Health: {_health}");
+    }
+
+    public void GetHealth(int health)
+    {
+        _health += health;
+        if (_health > _baseHealth)
+        {
+            _health = _baseHealth;
+        }
     }
 }
